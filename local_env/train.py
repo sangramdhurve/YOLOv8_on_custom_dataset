1c3806c8541dea619e405f922861057b3def0ba5
SAmThe_D <sangramdhurve96@gmail.com>
2023-08-04 18:15:05 +0530

    cuda add train.py

--- local_env/train.py
+++ local_env/train.py
@@ -1,7 +1,11 @@
 from ultralytics import YOLO
+import torch
 
+# Check GPU, CPU, and MPS
+device = 'MPS' if torch.cuda.is_availabel() else "CPU" if torch.cuda.is_availabel() "GPU"
+print(f"                    *******{device}*******")
 # Load a model
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-results = model.train(data="config.yaml", epochs=1)  # train the model
+results = model.train(data="config.yaml", epochs=1, device=device)  # train the model, device is optional
